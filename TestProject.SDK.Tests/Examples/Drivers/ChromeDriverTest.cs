34077728a9367e2cf4044ef86b595ad0c72d9f78
Bas Dijkstra <bas@ontestautomation.com>
2020-11-24 14:33:10 +0100

    Add Tests project and sample test showing the current capabilities of the SDK using Chrome
    Signed-off-by: Bas Dijkstra <bas@ontestautomation.com>
    
    Signed-off-by: Bas Dijkstra <bas@ontestautomation.com>

---
+++ TestProject.SDK.Tests/Examples/Drivers/ChromeDriverTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestProject.SDK.Drivers.Web;
+
+namespace TestProject.SDK.Tests.Examples.Drivers
+{
+    [TestClass]
+    public class ChromeDriverTest
+    {
+        [TestMethod]
+        public void ExampleTestUsingChromeDriver()
+        {
+            ChromeDriver driver = new ChromeDriver(
+                projectName: "My project",
+                jobName: "My job",
+                token: "aqqm_o3T_egvYLkI1eum8LV10IsHu-tKO3cRbJP6qW81");
+
+            driver.Quit();
+        }
+    }
+}
