71868a94d02499b02a070c0b500405685a339f59
Bas Dijkstra <bas@ontestautomation.com>
2020-11-24 14:32:22 +0100

    Payload objects for the session request and response
    Signed-off-by: Bas Dijkstra <bas@ontestautomation.com>
    
    Signed-off-by: Bas Dijkstra <bas@ontestautomation.com>

---
+++ TestProject.SDK/Internal/Rest/Messages/SessionResponse.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="SessionResponse.cs" company="TestProject">
+// Copyright 2020 TestProject (https://testproject.io)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Collections.Generic;
+
+namespace TestProject.SDK.Internal.Rest.Messages
+{
+    /// <summary>
+    /// Payload object returned by the Agent when starting a development session.
+    /// </summary>
+    public class SessionResponse
+    {
+        /// <summary>
+        /// Port number that Agent is listening on for the SDK to connect.
+        /// </summary>
+        public int DevSocketPort { get; set; }
+
+        /// <summary>
+        /// Remote address of a Selenium / Appium server for the driver to communicate with.
+        /// </summary>
+        public string ServerAddress { get; set; }
+
+        /// <summary>
+        /// ID of the session that has been initialized by the Agent.
+        /// </summary>
+        public string SessionId { get; set;  }
+
+        /// <summary>
+        /// Dialect of the session that has been initialized by the Agent.
+        /// </summary>
+        public string Dialect { get; set; }
+
+        /// <summary>
+        /// Capabilities of the session that has been initialized by the Agent.
+        /// </summary>
+        public Dictionary<string, object> Capabilities { get; set; }
+
+        /// <summary>
+        /// The current version of the Agent.
+        /// </summary>
+        public string Version { get; set; }
+    }
+}
