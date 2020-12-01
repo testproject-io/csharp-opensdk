deac49bdcf55f851564bf915bfb3d289fffd7006
Bas Dijkstra <bas@ontestautomation.com>
2020-12-01 17:28:28 +0100

    Update ChromeDriver example to reflect current SDK capabilities
    Signed-off-by: Bas Dijkstra <bas@ontestautomation.com>

--- TestProject.SDK.Tests/Examples/Drivers/ChromeDriverTest.cs
+++ TestProject.SDK.Tests/Examples/Drivers/ChromeDriverTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using TestProject.SDK.Drivers.Web;
 
 namespace TestProject.SDK.Tests.Examples.Drivers
@@ -6,14 +7,36 @@ namespace TestProject.SDK.Tests.Examples.Drivers
     [TestClass]
     public class ChromeDriverTest
     {
-        [TestMethod]
-        public void ExampleTestUsingChromeDriver()
+        private ChromeDriver driver;
+
+        [TestInitialize]
+        public void StartBrowser()
         {
-            ChromeDriver driver = new ChromeDriver(
+            OpenQA.Selenium.Chrome.ChromeOptions chromeOptions = new OpenQA.Selenium.Chrome.ChromeOptions();
+            chromeOptions.PageLoadStrategy = OpenQA.Selenium.PageLoadStrategy.Normal;
+            chromeOptions.UnhandledPromptBehavior = OpenQA.Selenium.UnhandledPromptBehavior.DismissAndNotify;
+
+            driver = new ChromeDriver(
+                chromeOptions: chromeOptions,
                 projectName: "My project",
                 jobName: "My job",
                 token: "aqqm_o3T_egvYLkI1eum8LV10IsHu-tKO3cRbJP6qW81");
+        }
 
+        [TestMethod]
+        public void ExampleTestUsingChromeDriver()
+        {
+            driver.Navigate().GoToUrl("https://example.testproject.io");
+            driver.FindElement(By.CssSelector("#name")).SendKeys("John Smith");
+            driver.FindElement(By.CssSelector("#password")).SendKeys("12345");
+            driver.FindElement(By.CssSelector("#login")).Click();
+
+            Assert.IsTrue(driver.FindElement(By.CssSelector("#greetings")).Displayed);
+        }
+
+        [TestCleanup]
+        public void CloseBrowser()
+        {
             driver.Quit();
         }
     }
