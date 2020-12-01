40f8da260117a3204e6cd04ffad1a6461acf7b0a
Bas Dijkstra <bas@ontestautomation.com>
2020-12-01 20:03:08 +0100

    Add extension methods class providing utilities for implementing custom business logic on standard types
    Signed-off-by: Bas Dijkstra <bas@ontestautomation.com>

---
+++ TestProject.SDK/Internal/Helpers/ExtensionMethods.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="ExtensionMethods.cs" company="TestProject">
+// Copyright 2020 TestProject (https://testproject.io)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using OpenQA.Selenium.Remote;
+
+namespace TestProject.SDK.Internal.Helpers
+{
+    /// <summary>
+    /// This class contains custom extension methods implementing SDK specific business logic.
+    /// </summary>
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// Determines whether or not a driver command should be patched before sending it to the Agent.
+        /// </summary>
+        /// <param name="driverCommand">The driver command that should or should not be patched.</param>
+        /// <returns>True if the driver command should be patched, false otherwise.</returns>
+        public static bool ShouldBePatched(this string driverCommand)
+        {
+            return driverCommand.Equals(DriverCommand.SendKeysToElement) || driverCommand.Equals(DriverCommand.SendKeysToActiveElement);
+        }
+    }
+}
