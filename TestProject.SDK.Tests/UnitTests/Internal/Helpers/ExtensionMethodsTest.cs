34fa36605eb63f1c1b2a877117618c5cd62c9366
Bas Dijkstra <bas@ontestautomation.com>
2020-12-01 20:03:29 +0100

    Add unit tests for extension method
    Signed-off-by: Bas Dijkstra <bas@ontestautomation.com>

---
+++ TestProject.SDK.Tests/UnitTests/Internal/Helpers/ExtensionMethodsTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Remote;
+using TestProject.SDK.Internal.Helpers;
+
+namespace TestProject.SDK.Tests.UnitTests.Internal.Helpers
+{
+    [TestClass]
+    public class ExtensionMethodsTest
+    {
+        [TestMethod]
+        public void ShouldBePatched_UsingSendKeysToElementCommand_ShouldReturnTrue()
+        {
+            string driverCommand = DriverCommand.SendKeysToElement;
+
+            Assert.IsTrue(driverCommand.ShouldBePatched());
+        }
+
+        [TestMethod]
+        public void ShouldBePatched_UsingSendKeysToActiveCommand_ShouldReturnTrue()
+        {
+            string driverCommand = DriverCommand.SendKeysToActiveElement;
+
+            Assert.IsTrue(driverCommand.ShouldBePatched());
+        }
+
+        [TestMethod]
+        public void ShouldBePatched_UsingClickElementCommand_ShouldReturnFalse()
+        {
+            string driverCommand = DriverCommand.ClickElement;
+
+            Assert.IsFalse(driverCommand.ShouldBePatched());
+        }
+    }
+}
