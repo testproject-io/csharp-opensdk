6038cebfb6223749f0a85b31a092e5ef84c92216
Bas Dijkstra <bas@ontestautomation.com>
2020-12-08 10:33:02 +0100

    Add support for RemoteWebDriver, with example
    Signed-off-by: Bas Dijkstra <bas@ontestautomation.com>

---
+++ TestProject.SDK.Tests/Examples/Drivers/RemoteWebDriverTest.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using TestProject.SDK.Drivers.Web;
+
+namespace TestProject.SDK.Tests.Examples.Drivers
+{
+    [TestClass]
+    public class RemoteWebDriverTest
+    {
+        private RemoteWebDriver driver;
+
+        [TestMethod]
+        public void ExampleTestUsingRemoteWebDriverWithChromeOptions()
+        {
+            OpenQA.Selenium.Chrome.ChromeOptions chromeOptions = new OpenQA.Selenium.Chrome.ChromeOptions();
+            chromeOptions.PageLoadStrategy = PageLoadStrategy.Normal;
+            chromeOptions.UnhandledPromptBehavior = UnhandledPromptBehavior.DismissAndNotify;
+
+            driver = new RemoteWebDriver(
+                driverOptions: chromeOptions,
+                projectName: "My project",
+                jobName: "My job",
+                token: "aqqm_o3T_egvYLkI1eum8LV10IsHu-tKO3cRbJP6qW81");
+
+            driver.Navigate().GoToUrl("https://example.testproject.io");
+            driver.FindElement(By.CssSelector("#name")).SendKeys("John Smith");
+            driver.FindElement(By.CssSelector("#password")).SendKeys("12345");
+            driver.FindElement(By.CssSelector("#login")).Click();
+
+            Assert.IsTrue(driver.FindElement(By.CssSelector("#greetings")).Displayed);
+        }
+
+        [TestMethod]
+        public void ExampleTestUsingRemoteWebDriverWithFirefoxOptions()
+        {
+            OpenQA.Selenium.Firefox.FirefoxOptions firefoxOptions = new OpenQA.Selenium.Firefox.FirefoxOptions();
+            firefoxOptions.PageLoadStrategy = PageLoadStrategy.Normal;
+            firefoxOptions.UnhandledPromptBehavior = UnhandledPromptBehavior.DismissAndNotify;
+
+            driver = new RemoteWebDriver(
+                driverOptions: firefoxOptions,
+                projectName: "My project",
+                jobName: "My job",
+                token: "aqqm_o3T_egvYLkI1eum8LV10IsHu-tKO3cRbJP6qW81");
+
+            driver.Navigate().GoToUrl("https://example.testproject.io");
+            driver.FindElement(By.CssSelector("#name")).SendKeys("John Smith");
+            driver.FindElement(By.CssSelector("#password")).SendKeys("12345");
+            driver.FindElement(By.CssSelector("#login")).Click();
+
+            Assert.IsTrue(driver.FindElement(By.CssSelector("#greetings")).Displayed);
+        }
+
+        [TestCleanup]
+        public void CloseBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
