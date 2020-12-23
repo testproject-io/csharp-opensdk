a454db967f9d8fdc718df115f7aff044b1cf9669
Bas Dijkstra <bas@ontestautomation.com>
2020-12-23 17:22:04 +0100

    Add interface to be implemented by all command executors
    Signed-off-by: Bas Dijkstra <bas@ontestautomation.com>

---
+++ TestProject.OpenSDK/Internal/Helpers/CommandExecutors/ITestProjectCommandExecutor.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="ITestProjectCommandExecutor.cs" company="TestProject">
+// Copyright 2020 TestProject (https://testproject.io)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using OpenQA.Selenium.Remote;
+
+namespace TestProject.OpenSDK.Internal.Helpers.CommandExecutors
+{
+    /// <summary>
+    /// Interface to be implemented by all TestProject command executors.
+    /// </summary>
+    public interface ITestProjectCommandExecutor
+    {
+        /// <summary>
+        /// Extended command execution method.
+        /// Allows skipping reporting for "internal" commands, for example:
+        /// - Taking screenshot for manual step reporting.
+        /// - Inspecting element type to determine whether redaction is required.
+        /// </summary>
+        /// <param name="commandToExecute">The WebDriver command to execute.</param>
+        /// <param name="skipReporting">True if the command should not be reported, false otherwise.</param>
+        /// <returns>The <see cref="Response"/> returned by the Agent.</returns>
+        Response Execute(Command commandToExecute, bool skipReporting);
+    }
+}
