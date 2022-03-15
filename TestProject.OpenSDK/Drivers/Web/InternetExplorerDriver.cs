924119ae5c6a314b5959d85868e1d455359e8844
Tyler Poradun <tporadun@jettyapp.com>
2022-03-16 02:47:47 +0900

    fix: use correct browser type for InternetExplorerDriver
    Signed-off-by: Tyler Poradun <typoradun@comcast.net>

--- TestProject.OpenSDK/Drivers/Web/InternetExplorerDriver.cs
+++ TestProject.OpenSDK/Drivers/Web/InternetExplorerDriver.cs
@@ -54,7 +54,7 @@ namespace TestProject.OpenSDK.Drivers.Web
             string reportPath = null,
             TimeSpan? remoteConnectionTimeout = null,
             int restClientTimeout = AgentClient.DefaultRestClientTimeoutInMilliseconds)
-            : base(remoteAddress, token, DriverOptionsHelper.Patch(internetExplorerOptions, BrowserType.Chrome), projectName, jobName, disableReports, reportType, reportName, reportPath, remoteConnectionTimeout, restClientTimeout)
+            : base(remoteAddress, token, DriverOptionsHelper.Patch(internetExplorerOptions, BrowserType.InternetExplorer), projectName, jobName, disableReports, reportType, reportName, reportPath, remoteConnectionTimeout, restClientTimeout)
         {
         }
     }
